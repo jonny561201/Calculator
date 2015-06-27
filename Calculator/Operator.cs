5baf36333c8d0e364b0cf99bc27a705f92946df5
Jon Graf <jongraf1@gmail.com>
2015-06-26 21:51:49 -0500

    Initial setup and generation of test and operation classes.  Created first test to consolidate character components into string.

---
+++ Calculator/Operator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Operator
+    {
+        private static string userInput;
+
+        internal static string Combine(string p1)
+        {
+            userInput += p1;
+
+            return userInput;
+        }
+    }
+}
---
+++ Calculator/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void addTwoFiguresTogether()
+        {
+            string testResult = Operator.Combine("1");
+            testResult = Operator.Combine("2");
+            Assert.AreEqual("12",testResult);
+        }
+
+        [Test]
+        public void numberStartsWithZero()
+        {
+
+        }
+    }
+}
